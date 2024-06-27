8e9c91f13cbd24d8e6aad45fa7e31a2ba265dd1e
pedaaro <pedrocorsi@live.com>
2024-06-27 20:40:39 -0300

    last header details added

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,10 +7,13 @@ import  Link  from 'next/link'
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
 import { HiDownload } from 'react-icons/hi'
 import { FaGithubSquare } from 'react-icons/fa'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Intro() {
+  const { ref } = useSectionInView('Home', 0.5);
+  
   return (
-    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+    <section ref={ref} id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
      <div className='flex items-center justify-center'>
       <div className='relative'>
        <motion.div
@@ -27,7 +30,7 @@ export default function Intro() {
         width={192} 
         height={192} 
         quality={75} 
-        className='h-192 w-192 rounded-2xl object-cover border-[0.15rem] border-white shadow-xl'
+        className='h-192 w-192 mt-[-2rem] rounded-2xl object-cover border-[0.15rem] border-white shadow-xl'
         />
         </motion.div>
        <motion.span className='absolute -bottom-3.5 -right-3.5  text-4xl'
@@ -54,12 +57,14 @@ export default function Intro() {
      transition={{
       delay: 0.1
      }}>
-      <Link href="#contact" className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition'>Contact me here 
-      <BsArrowRight />
+      <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition'>
+      Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
       </Link>
-      <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>Download CV <HiDownload /></a>
-      <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full'><BsLinkedin /></a>
-      <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full'><FaGithubSquare /></a>
+      <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/30' href='/Pedro JavaScript Developer CV.pdf' download>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition cursor-pointer'/></a>
+
+      <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/30' href='https://www.linkedin.com/in/pedrocorsin/' target='_blank'><BsLinkedin /></a>
+
+      <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/30' href='https://github.com/pedaaro' target='_blank'><FaGithubSquare /></a>
      </motion.div>
     </section>
   )
