33b0c1fa47af264e0cc77f4a549b1ea7032211f9
pedaaro <pedrocorsi@live.com>
2024-07-04 11:34:12 -0300

    image variable removed

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@
 const nextConfig = {
  basePath: "/portfolio",
  output: "export",
- reactStrictMode: true,
- images: {
-  loader: 'custom',
-  loaderFile: ['./public/projectPic2.png', './public/pedro-corsi2.jpg']
- }
+ reactStrictMode: true
 }
 
-export default nextConfig
+module.exports = nextConfig
